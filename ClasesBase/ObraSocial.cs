17c9cc8813a22cfae381c0abcff38b39c609d4a5
mauromamani <mauromamani.b@gmail.com>
2022-05-03 19:53:43 -0300

    agregar diagrama de clases, implementar diagrama

--- ClasesBase/ObraSocial.cs
+++ ClasesBase/ObraSocial.cs
@@ -5,7 +5,35 @@ using System.Text;
 
 namespace ClasesBase
 {
-    class ObraSocial
+    public class ObraSocial
     {
+        private string OS_CUIT;
+        private string OS_RazonSocial;
+        private string OS_Telefono;
+        private string OS_Direccion;
+
+        public string OS_CUIT1
+        {
+            get { return OS_CUIT; }
+            set { OS_CUIT = value; }
+        }
+
+        public string OS_RazonSocial1
+        {
+            get { return OS_RazonSocial; }
+            set { OS_RazonSocial = value; }
+        }
+
+        public string OS_Direccion1
+        {
+            get { return OS_Direccion; }
+            set { OS_Direccion = value; }
+        }
+
+        public string OS_Telefono1
+        {
+            get { return OS_Telefono; }
+            set { OS_Telefono = value; }
+        }
     }
 }
--- ClasesBase/Producto.cs
+++ ClasesBase/Producto.cs
@@ -5,7 +5,35 @@ using System.Text;
 
 namespace ClasesBase
 {
-    class Producto
+    public class Producto
     {
+        private string prod_Codigo;
+        private string prod_Categoria;
+        private string prod_Descripcion;
+        private decimal prod_Precio;
+
+        public string Prod_Codigo
+        {
+            get { return prod_Codigo; }
+            set { prod_Codigo = value; }
+        }
+      
+        public string Prod_Categoria
+        {
+            get { return prod_Categoria; }
+            set { prod_Categoria = value; }
+        }
+
+        public string Prod_Descripcion
+        {
+            get { return prod_Descripcion; }
+            set { prod_Descripcion = value; }
+        }
+        
+        public decimal Prod_Precio
+        {
+            get { return prod_Precio; }
+            set { prod_Precio = value; }
+        }
     }
 }
