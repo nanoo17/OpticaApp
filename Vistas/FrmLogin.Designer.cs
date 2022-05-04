c3f337ee8aa3bc223327e06b605abaa6db7c4d1c
mauromamani <mauromamani.b@gmail.com>
2022-05-04 17:19:04 -0300

    bugs del form login

--- Vistas/FrmLogin.Designer.cs
+++ Vistas/FrmLogin.Designer.cs
@@ -81,7 +81,6 @@
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(156, 20);
             this.txtPassword.TabIndex = 4;
-            this.txtPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // bienvenida
             // 
@@ -107,7 +106,6 @@
             this.Controls.Add(this.Aceptar);
             this.Name = "FrmLogin";
             this.Text = "FrmLogin";
-            this.Load += new System.EventHandler(this.FrmLogin_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
