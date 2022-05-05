ea3013f1829f00780536ff0b522c6c4667cc8586
mauromamani <mauromamani.b@gmail.com>
2022-05-04 21:06:27 -0300

    agregar formulario de producto

--- Vistas/FrmProducto.cs
+++ Vistas/FrmProducto.cs
@@ -15,5 +15,37 @@ namespace Vistas
         {
             InitializeComponent();
         }
+
+        private void button_Cargar_Click(object sender, EventArgs e)
+        {
+            // Parametros del messageBox
+            string mensaje = "¿Quiere guardar el producto?";
+            string titulo = "Guardar producto";
+            MessageBoxButtons botones = MessageBoxButtons.YesNo;
+            MessageBoxIcon icono = MessageBoxIcon.Question;
+
+            // Mostrar messageBox de confirmación
+            DialogResult resultado = MessageBox.Show(mensaje, titulo, botones, icono);
+            
+            // Verificar el resultado del messageBox
+            if (resultado == DialogResult.No)
+            {
+                MessageBox.Show("La operación fue cancelada", "Guardar producto");
+                return;                
+            }
+
+            // Se procede al almacenamiento del producto
+
+            string prodCodigo = textBox_ProdCodigo.Text;
+            string prodCategoria = textBox_ProdCategoria.Text;
+            string prodDescripcion = textBox_ProdDescripcion.Text;
+            // TODO: Manejo de errores en el decimal
+            decimal prodPrecio = Convert.ToDecimal(textBox_ProdPrecio.Text);
+
+            // TODO: Validaciones de los campos ingresados
+
+            // TODO: Constructor de productos
+            MessageBox.Show(prodDescripcion, "Nuevo Producto");
+        }
     }
 }
