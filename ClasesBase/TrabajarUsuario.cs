88db2f16a4ae63f3a1744fa9af66b056496810fa
nanoo17 <marianojrodriguez10gmail.com>
2022-05-19 14:13:11 -0300

    agregado de trabajar Usuario

--- ClasesBase/TrabajarUsuario.cs
+++ ClasesBase/TrabajarUsuario.cs
@@ -2,10 +2,92 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ClasesBase
 {
     class TrabajarUsuario
     {
+
+        public static void insertarUsuario(Usuario usuario)
+        {
+            SqlConnection db = new SqlConnection(ClasesBase.Properties.Settings.Default.opticaConnectionString);
+
+            SqlCommand query = new SqlCommand();
+            query.CommandText = "INSERT INTO Usuario (Usu_ID,Usu_NombreUsuario,Usu_Contraseña,Usu_ApellidoNombre,Rol_Codigo) VALUES (@id, @usuario,@contraseña, @apellidoNombre, @rol)";
+            query.CommandType = CommandType.Text;
+            query.Connection = db;
+
+            // Paramatros
+            query.Parameters.AddWithValue("@id", usuario.Usu_ID);
+            query.Parameters.AddWithValue("@usuario", usuario.Usu_NombreUsuario);
+            query.Parameters.AddWithValue("@contraseña", usuario.Usu_Clave);
+            query.Parameters.AddWithValue("@apellidoNombre", usuario.Usu_ApellidoNombre);
+            query.Parameters.AddWithValue("@rol", usuario.Rol);
+           
+            // Ejecutar la query
+            db.Open();
+            query.ExecuteNonQuery();
+            db.Close();
+        }
+
+        public static DataTable obtenerUsuarios()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.opticaConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM Usuario";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+
+        public static void eliminarUsuario(string id)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.opticaConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "DELETE FROM Usuario WHERE Usu_ID = @id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            // Paramatros
+            cmd.Parameters.AddWithValue("@id", id);
+
+            // Ejecutar la query
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public static void modificarUsuario(Usuario usuario)
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.opticaConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE Usuario SET Usu_ID = @id,Usu_NombreUsuario = @usuario,Usu_Contraseña = @contraseña,Usu_ApellidoNombre = @apellidoNombre,Rol_Codigo = @rol WHERE Usu_ID = @id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            // Paramatros
+            cmd.Parameters.AddWithValue("@id", usuario.Usu_ID);
+            cmd.Parameters.AddWithValue("@usuario", usuario.Usu_NombreUsuario);
+            cmd.Parameters.AddWithValue("@contraseña", usuario.Usu_Clave);
+            cmd.Parameters.AddWithValue("@apellidoNombre", usuario.Usu_ApellidoNombre);
+            cmd.Parameters.AddWithValue("@rol", usuario.Rol);
+            // Ejecutar la query
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+
     }
 }
