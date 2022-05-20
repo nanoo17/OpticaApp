8542aa4b85a2ebfd78999caf7b55353e1ae268d0
mauromamani <mauromamani.b@gmail.com>
2022-05-19 22:04:12 -0300

    form venta y ventaDetalle

---
+++ ClasesBase/TrabajarProducto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    public class TrabajarProducto
+    {
+        public static DataTable obtenerProductos()
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.opticaConnectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT * FROM Producto";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+    }
+}
---
+++ ClasesBase/VentaDetalles.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class VentaDetalle
+    {
+        private int det_Nro;
+
+        public int Det_Nro
+        {
+            get { return det_Nro; }
+            set { det_Nro = value; }
+        }
+        private int ven_Nro;
+
+        public int Ven_Nro
+        {
+            get { return ven_Nro; }
+            set { ven_Nro = value; }
+        }
+        private string prod_Codigo;
+
+        public string Prod_Codigo
+        {
+            get { return prod_Codigo; }
+            set { prod_Codigo = value; }
+        }
+        private decimal det_Precio;
+
+        public decimal Det_Precio
+        {
+            get { return det_Precio; }
+            set { det_Precio = value; }
+        }
+        private decimal det_Cantidad;
+
+        public decimal Det_Cantidad
+        {
+            get { return det_Cantidad; }
+            set { det_Cantidad = value; }
+        }
+        private decimal det_Total;
+
+        public decimal Det_Total
+        {
+            get { return det_Total; }
+            set { det_Total = value; }
+        }
+    }
+}
